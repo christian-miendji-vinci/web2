d3ef67f56e94eb74232da5f34c5185233f9210d5
christian-miendji-vinci <christian.miendji@student.vinci.be>
2025-09-30 20:43:43 +0200

    new: exo1.3

--- tutorials/back/api/basic/routes/films.ts
+++ tutorials/back/api/basic/routes/films.ts
@@ -33,4 +33,57 @@ router.get("/" ,(_req ,res) =>{
   return res.json(films) ;
 })
 
+router.get("/:id" ,(req,res) => {
+  const id = Number(req.params.id) ;
+  const film = films.find((film) => film.id === id) ;
+  if(!film){
+    return res.sendStatus(404) ;
+  }
+  return res.json(film) ;
+})
+
+type NewFilm = Omit<Film , "id"> ;
+export type {NewFilm} ;
+
+router.post("/" , (req ,res) =>{
+   const body : unknown = req.body ;
+   if(
+    !body ||
+    typeof body !== "object" ||
+    !("title" in  body) ||
+    !("director" in body) ||
+    !("duration" in body) ||
+    typeof body.title !== "string" ||
+    typeof body.director !== "string" ||
+    typeof body.duration  !== "number" ||
+    ! body.title.trim() ||
+    ! body.director.trim() ||
+     body.duration <= 0
+   ){
+     return res.sendStatus(400) ;
+   }
+   const {title , director , duration } = body as NewFilm ;
+   const nextId = films.reduce((maxId , film) => (film.id > maxId ? film.id : maxId) , 0) + 1;
+   const newFilm : Film = {
+       id : nextId,
+       title ,
+       director ,
+       duration ,
+       
+   }
+   films.push(newFilm) ;
+   return res.json(newFilm) ;
+})
+
+router.get("/",(req , res) =>{
+  if(!req.query["minimun-duration"]){
+    return res.json(films) ;
+  }
+  const minDuration = Number(req.query["minimun-duration"]) ;
+  const filteredFilm =films.filter((film) =>{
+    return film.duration <= minDuration ;
+  });
+  return res.json(filteredFilm) ;
+})
+
 export default router 
