ea5d410b5f25e8009b20605852e9631a7711380c
christian-miendji-vinci <christian.miendji@student.vinci.be>
2025-12-02 12:12:32 +0100

    new:exo2.14

--- tutorials/exercices/2.13b/src/App.tsx
+++ tutorials/exercices/2.13b/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import RamdomDog from "./Ramdog";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [refresh, setRefresh] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>CHOOSE YOURS DOGS </h1>
+     
+      <div style={{ display: "flex", flexDirection: "column", gap: 200 }}>
+        <RamdomDog key={`${refresh}1`} />
+        <RamdomDog key={`${refresh}2`} />
+        <RamdomDog key={`${refresh}3`} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <button
+        onClick={() => setRefresh(!refresh)}
+         style={{
+          marginTop: "20px",
+          padding: "10px 20px",
+          fontSize: "1em",
+          cursor: "pointer",
+        }}
+      >
+       DOGS
+      </button>
+    </>
+  );
+};
+
+export default App;
